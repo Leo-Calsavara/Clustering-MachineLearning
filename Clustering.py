e53fc60d4bcb9497e52822b2fee53b543cffbb39
isacoelhou <isadora.araujo1@unioeste.br>
2024-09-27 09:15:59 -0300

    avaliação do kmeans

--- Clustering.py
+++ Clustering.py
@@ -10,6 +10,9 @@ from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import metrics
+from sklearn.metrics import pairwise_distances
+from sklearn.metrics import completeness_score
+import numpy as np
 
 
 data = pd.read_csv("./dataset/teste.csv")
@@ -49,34 +52,35 @@ for n_clusters in (1,2,3,4,5,6):
 kmeans = KMeans(n_clusters=best_i, max_iter=best_j, random_state=0)
 kmeans.fit(X)
 
-print("\nSoma dos quadrados das distâncias até o centróide mais próximo: ",kmeans.inertia_)
-
 print("\nCoesão: ", math.sqrt(kmeans.inertia_)/kmeans.n_clusters)
 
 print("\nCoeficiente de Silhueta: ",metrics.silhouette_score(X,kmeans.labels_))
 
-print("\nRand Score K-means: ",metrics.rand_score(y,kmeans.labels_))
-
 print("\nHomogeneidade : ",metrics.homogeneity_score(y,kmeans.labels_))
 
-print("\nCompletude : ",metrics.completeness_score(y,kmeans.labels_))
+print("\nSeparação: ", pairwise_distances(kmeans.cluster_centers_).mean())
+
+print("\nRand Score K-means: ",metrics.rand_score(y, kmeans.labels_))
+
+print("\nCompletude: ", completeness_score(y, kmeans.labels_))
+
+print("\nEntropia: ", entropy(np.bincount(kmeans.labels_)/len(kmeans.labels_)))
+
+# best = -1
+# best
+# for eps in (0.1, 0.2, 0.3, 0.4, 0.5):
+#     for min_samples in (3, 5, 7, 10):
+#         dbscan = DBSCAN(eps=eps, min_samples=min_samples)
+#         dbscan.fit(X)
+
+#         labels = dbscan.labels_
 
 
-best = -1
-best
-for eps in (0.1, 0.2, 0.3, 0.4, 0.5):
-    for min_samples in (3, 5, 7, 10):
-        dbscan = DBSCAN(eps=eps, min_samples=min_samples)
-        dbscan.fit(X)
 
-        labels = dbscan.labels_
+# for n_clusters in (2, 3, 4, 5, 6):
+#     for linkage in ('ward', 'complete', 'average', 'single'):
+#         agnes = AgglomerativeClustering(n_clusters=n_clusters, linkage=linkage)
+#         agnes.fit(X)
 
-
-
-for n_clusters in (2, 3, 4, 5, 6):
-    for linkage in ('ward', 'complete', 'average', 'single'):
-        agnes = AgglomerativeClustering(n_clusters=n_clusters, linkage=linkage)
-        agnes.fit(X)
-
-        labels = agnes.labels_
+#         labels = agnes.labels_
 
